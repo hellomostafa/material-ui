5db8ae45f8d36c1914d76a7477ed6fc8c0c73ee7
iammostafa76 <iamamirmostafa@gmail.com>
2021-10-17 09:05:33 +0600

    typography added

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 const Create = () => {
     return (
         <div>
             <Typography 
-                variant="h1"
-                color="primary"
-                align="center"
+                variant="h6"
+                color="textSecondary"
+                component="h2"
+                gutterBottom
             >
                 Create a New Note
             </Typography>
+
+            <Button type="submit" variant="outlined" color="secondary"> 
+                Buy Now
+            </Button>
+
+            <ButtonGroup variant="contained" color="secondary" aria-label="">
+              <Button>See more</Button>
+              <Button>Buy Now</Button>
+              <Button>Clicked</Button>
+              <Button>Filtered</Button>
+              
+            </ButtonGroup>
         </div>
     );
 };
