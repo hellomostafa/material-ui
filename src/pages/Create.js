860685f131a7733340d4de77e203250e3dfe93a8
iammostafa76 <iamamirmostafa@gmail.com>
2021-10-17 09:56:17 +0600

    update 1.0

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import ButtonGroup from '@mui/material/ButtonGroup'
+import Container from '@mui/material/Container'
 
 const Create = () => {
     return (
-        <div>
+        <Container>
+          
+        
             <Typography 
                 variant="h6"
                 color="textSecondary"
@@ -15,18 +18,14 @@ const Create = () => {
                 Create a New Note
             </Typography>
 
-            <Button type="submit" variant="outlined" color="secondary"> 
+            <Button 
+                type="submit" 
+                variant="outlined" 
+                color="secondary"
+            > 
                 Buy Now
             </Button>
-
-            <ButtonGroup variant="contained" color="secondary" aria-label="">
-              <Button>See more</Button>
-              <Button>Buy Now</Button>
-              <Button>Clicked</Button>
-              <Button>Filtered</Button>
-              
-            </ButtonGroup>
-        </div>
+        </Container>
     );
 };
 
